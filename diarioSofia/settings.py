d9966addc12ea0c78abea40141f51b4bf483da88
rodrigo delfino de carvalho <rodrigoddc1@gmail.com>
2019-05-12 01:22:51 -0300

    Preparando heroku 2

--- diarioSofia/settings.py
+++ diarioSofia/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'diario-da-gabi.herokuapp.com',
+]
 
 
 # Application definition
