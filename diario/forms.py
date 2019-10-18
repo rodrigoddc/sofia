a9b688c9184eae5dd1f8795fcb4a3d0d1ce52bde
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2019-10-18 08:02:44 -0300

    att

---
+++ diario/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from diario.models import Imagem
+
+
+class ImagemForm(forms.Form):
+    titulo = forms.CharField(label='Título')
+    descricao = forms.CharField(label='Descrição')
+    imagem = forms.ImageField(label='Imagem')
+    album_id = forms.CharField(label='ID do Álbum')
+
+
+class ImagemModelForm(forms.ModelForm):
+    class Meta:
+        model = Imagem
+        fields = ['titulo', 'descricao', 'imagem']
