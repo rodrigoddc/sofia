2a1fea56827c601f6185f129cde15cdaaf914bc8
rodrigo <rodrigoddc1@gmail.com>
2019-11-06 11:07:20 -0300

    test

--- diario/views.py
+++ diario/views.py
@@ -14,32 +14,34 @@ class ListAlbuns(ListView):
         albuns = Album.objects.all()
         return render(request, 'diario/lista-albuns.html', {'albuns': albuns})
 
-    def post(self, request):
-
-        data = {
-            'form_item': ImagemForm(),
-            'titulo': request.POST['titulo'],
-            'descricao': request.POST['descricao'],
-            'imagem': request.POST['imagem'],
-            'album_id': request.POST['album_id']}
-
-        if data['album_id']:
-            album = Album.objects.get(id=data['album_id'])
-            data['titulo'] = request.POST['titulo']
-            data['descricao'] = request.POST['descricao']
-            data['imagem'] = request.POST['imagem']
-            album.save()
-        else:
-            album = Album.objects.create(
-                titulo=data['titulo'],
-                descricao=data['descricao'],
-                imagem=data['imagem'],)
-
-        imagens = album.imagem_set.all()
-        data['album'] = album
-        data['itens'] = imagens
-
-        return render(request, 'diario/album-detail.html', data)
+    # def post(self, request):
+    #
+    #     data = {
+    #         'form_item': ImagemForm(),
+    #         'titulo': request.POST['titulo'],
+    #         'descricao': request.POST['descricao'],
+    #         'imagem': request.POST['imagem'],
+    #         'album_id': request.POST['album_id'],
+    #         'usuario': request.user
+    #     }
+    #
+    #     if data['album_id']:
+    #         album = Album.objects.get(id=data['album_id'])
+    #         data['titulo'] = request.POST['titulo']
+    #         data['descricao'] = request.POST['descricao']
+    #         data['imagem'] = request.POST['imagem']
+    #         album.save()
+    #     else:
+    #         album = Album.objects.create(
+    #             titulo=data['titulo'],
+    #             descricao=data['descricao'],
+    #             imagem=data['imagem'],)
+    #
+    #     imagens = album.imagem_set.all()
+    #     data['album'] = album
+    #     data['itens'] = imagens
+    #
+    #     return render(request, 'diario/album-detail.html', data)
 
 
 class AlbumDetail(LoginRequiredMixin, DetailView):
