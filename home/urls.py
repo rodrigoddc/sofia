1b57282c9b5a0fd03c94224ef152fdddbfe4c383
rodrigo <rodrigoddc1@gmail.com>
2019-11-08 17:56:16 -0300

    fixup! asd

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import HomePageView, Index, Profile
+from .views import HomePageView, Index, Profile, Login
 
 app_name = 'home'
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('index/', Index.as_view(), name='index'),
     path('profile/', Profile.as_view(), name='profile'),
-
+    path('login/', Login.as_view(), name='login'),
 ]
